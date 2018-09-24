d40d74c5b4cd9a1b7e1e12dceab7ae4a5091105b
Andres Conde <andresconder@gmail.com>
2018-09-23 22:41:26 -0500

    Added app.js methods

--- k-means-algorithm/src/App.js
+++ k-means-algorithm/src/App.js
@@ -3,12 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props)
+  {
+      super(props);
+      this.state = {
+          dataSet: '',
+          kMeans:''
+      };
+  }
+
+
+//Kmeans Algorithm
+
+//Read data file
+readFile(fileName)
+{
+
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <p>¿Cuantas K desea utilizar?</p>
+          <input type="number" name="kMeans" value={this.state.kMeans} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
