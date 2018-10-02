b6b423cfa7ff671e25270470d588f1e0bc2a283d
57LECS <eddsc57@gmail.com>
2018-10-02 16:54:42 -0500

    Tab bar implementation

--- k-means-algorithm/src/App.js
+++ k-means-algorithm/src/App.js
@@ -7,9 +7,26 @@ import './App.css';
 import Button from '@material-ui/core/Button';  
 import TextField from '@material-ui/core/TextField'
 import Input from '@material-ui/core/Input';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PropTypes from 'prop-types';
 import Kmeans from './Kmeans';
 import Vectors from './Vectors';
+import Typography from '@material-ui/core/Typography';
 
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 const styles = theme => ({
   container: {
@@ -57,31 +74,33 @@ setAlgorithm(event)
 
 }
 
+handleChange = (event, value) => {
+  this.setState({ value });
+};
 
 
   render() {  
     var state = this.state["Algorithm"]
-    if(state)
+    const { value } = this.state;
       return (
-
           <div>
-            <button onClick={this.setAlgorithm}>Cambiar</button>
-            <Kmeans/>
+            <AppBar position="static">
+                <Tabs value={value} onChange={this.handleChange}>
+                  <Tab value="one" label="kMeans" />
+                  <Tab value="two" label="Vectores" />
+                </Tabs>
+              </AppBar>
+              {value === 'one' && <TabContainer>
+                <h1>Kmeans</h1>
+                <Kmeans/>
+              </TabContainer>}
+              {value === 'two' && <TabContainer>
+                <h1>Vectors</h1>
+                <Vectors/>
+              </TabContainer>}
           </div>
-          
         );
-        else
-          return  (
-
-            <div>
-              <button onClick={this.setAlgorithm}>Cambiar</button>
-              <Vectors/>
-            </div>
-            
-          );
-       
-
   }
 }
 
-export default App;
+export default App; 
